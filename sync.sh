d64f2a4ca9b89bf8b42aefcc8ecdcbd1d8900eb0
Earl Sioson <earlsioson@users.noreply.github.com>
2022-08-11 23:58:25 -0400

    add kitty config

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 rsync -av --delete --exclude plugin/packer_compiled.lua ~/.config/nvim/ .config/nvim/
 rsync -av --delete --exclude plugged --exclude plug.vim.old ~/.vim/ .vim/
+rsync -av --delete ~/.config/kitty/ .config/kitty/
 rsync -av  --delete ~/.vimrc .vimrc
 rsync -av  --delete ~/.tmux.conf .tmux.conf

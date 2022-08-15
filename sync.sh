c133077ef9d27b1f50e3ae588a27d786960ec795
Earl Sioson <earlsioson@users.noreply.github.com>
2022-08-15 07:54:45 -0400

    better tmux, no kitty

--- sync.sh
+++ sync.sh
@@ -1,5 +1,4 @@
 rsync -av --delete --exclude plugin/packer_compiled.lua ~/.config/nvim/ .config/nvim/
 rsync -av --delete --exclude plugged --exclude plug.vim.old ~/.vim/ .vim/
-rsync -av --delete ~/.config/kitty/ .config/kitty/
 rsync -av  --delete ~/.vimrc .vimrc
 rsync -av  --delete ~/.tmux.conf .tmux.conf

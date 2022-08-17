f76cd9432d1bcb7099a3941a053dbbd63a973248
Earl Sioson <earlsioson@users.noreply.github.com>
2022-08-17 14:24:13 -0400

    reverse sync

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-rsync -av --delete --exclude plugin/packer_compiled.lua ~/.config/nvim/ .config/nvim/
-rsync -av --delete --exclude plugged --exclude plug.vim.old ~/.vim/ .vim/
-rsync -av  --delete ~/.vimrc .vimrc
-rsync -av  --delete ~/.tmux.conf .tmux.conf
+rsync -av --delete --exclude plugin/packer_compiled.lua .config/nvim/ ~/.config/nvim/
+rsync -av --delete --exclude plugged --exclude plug.vim.old .vim/ ~/.vim/
+rsync -av  .vimrc ~/.vimrc
+rsync -av  .tmux.conf ~/.tmux.conf

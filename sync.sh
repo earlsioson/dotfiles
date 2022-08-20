c110835a6e51a57169a394ea499cffe1eecd089c
Earl Sioson <earlsioson@users.noreply.github.com>
2022-08-20 10:31:43 -0400

    remove sync

--- sync.sh
+++
@@ -1,4 +0,0 @@
-rsync -av --delete --exclude plugin/packer_compiled.lua .config/nvim/ ~/.config/nvim/
-rsync -av --delete --exclude plugged --exclude plug.vim.old .vim/ ~/.vim/
-rsync -av  .vimrc ~/.vimrc
-rsync -av  .tmux.conf ~/.tmux.conf

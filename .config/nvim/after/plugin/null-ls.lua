660e93ea13645fde9eb5190d129975501619f97e
Earl Sioson <earlsioson@users.noreply.github.com>
2022-08-29 07:16:02 -0400

    add format on save

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -2,7 +2,7 @@ local has_null_ls, null_ls = pcall(require, "null-ls")
 if not has_null_ls then
   return
 end
-
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
   sources = {
     null_ls.builtins.code_actions.eslint_d,
@@ -31,5 +31,18 @@ null_ls.setup({
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.taplo,
     null_ls.builtins.formatting.yamlfmt
-  }
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 })

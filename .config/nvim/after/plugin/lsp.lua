babdb1df627252d374072d5d1d8de9328c899ca0
Earl Sioson <earlsioson@users.noreply.github.com>
2022-08-29 11:50:27 -0400

    remove null-ls

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -150,6 +150,11 @@ vim.api.nvim_create_autocmd({"BufWritePre"}, {
   pattern = {"*.tf", "*.tfvars"},
   callback = vim.lsp.buf.formatting_sync,
 })
+lspconfig.eslint.setup{}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.ts", "*.tsx", "*.js", "*.jsx"},
+  command = "EslintFixAll"
+})
 lspconfig.taplo.setup{}
 lspconfig.rust_analyzer.setup{
   capabilities = capabilities,

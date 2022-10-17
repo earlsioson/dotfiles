58750407405f43a813ecb4b5582f8bfda033d35d
Earl Sioson <earlsioson@users.noreply.github.com>
2022-10-17 09:47:25 -0400

    use cmp_nvim_lsp updated api method

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -75,7 +75,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 vim.diagnostic.config {
   float = { border = "single" },
 }

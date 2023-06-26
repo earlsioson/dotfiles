94f979edcb65255b219721ad05dac124ac584042
Earl Sioson <earlsioson@users.noreply.github.com>
2023-06-26 17:20:27 -0400

    copilot tweaks

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -1,2 +1,6 @@
+vim.g.copilot_enabled = false
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+local opt = { noremap = true, silent = true }
+vim.keymap.set("n", "<Leader>cp", "<Cmd>Copilot enable<CR>", opt)
+vim.keymap.set("n", "<Leader>cP", "<Cmd>Copilot disable<CR>", opt)
